7ccd76cd2ab284f32d650f84e527d4d3dd856228
Crowdbotics <team@crowdbotics.com>
2024-02-05 19:23:41 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "biometricauthhealth-45972",
+  url: "https://biometricauthhealth-45972.botics.co",
+  api: "https://biometricauthhealth-45972.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
